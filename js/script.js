aef030e40c6e79d5f8be3d0ed6629bc73cf83ff9
mitali shah <mitalishaleenshah@gmail.com>
2020-07-09 09:31:02 -0700

    accessing the user's input on click

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const entryForm = document.getElementById("entryForm");
+const entriesSection = document.getElementById("entries");
+const entryTextbox = document.querySelector(".entry-textbox");
 
 let addEntryToDom = function (event) {
   event.preventDefault();
+  console.log(entryTextbox.value);
 };
 
 entryForm.addEventListener("submit", addEntryToDom);
