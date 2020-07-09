eea50dc386062f35a05c044ff7a338d2ae224e77
mitali shah <mitalishaleenshah@gmail.com>
2020-07-09 10:35:50 -0700

    added navigation button for each entry

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const entryForm = document.getElementById("entryForm");
-const entriesSection = document.getElementById("entries");
+const entriesSection = document.querySelector("#entries");
 const entryTextbox = document.querySelector(".entry-textbox");
+const entriesNav = document.querySelector(".entries-nav");
 
 let addEntryToDom = function (event) {
   event.preventDefault();
   const entryDiv = document.createElement("div");
   entryDiv.className = "single-entry";
   entryDiv.innerText = entryTextbox.value;
+  entryDiv.style.display = "none";
   entriesSection.appendChild(entryDiv);
+  entryTextbox.value = "";
+
+  const displayEntryButton = document.createElement("button");
+  displayEntryButton.className = "display-entry-button";
+  displayEntryButton.innerText = 1;
+  entriesNav.appendChild(displayEntryButton);
 };
 
 entryForm.addEventListener("submit", addEntryToDom);
